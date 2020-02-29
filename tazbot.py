1b993c510c6d8dda72b43a0a286b3dd6f91f4054
Tom Knuf <t-knuf@hotmail.de>
2020-02-29 16:18:11 +0100

    Save articles every hour and send 8 of them each day

--- tazbot.py
+++ tazbot.py
@@ -7,7 +7,18 @@ import time
 import os
 import datetime
 
-def tazArticles():
+print(datetime.datetime.now())
+
+token = os.environ["telegramToken"]
+bot = telegram.Bot(token=token)
+print(bot.get_me())
+
+COLLECTION = {}
+
+def scrape():
+    print("Scraping...")
+    global COLLECTION
+
     url = "https://taz.de"
 
     website = requests.get(url)
@@ -16,31 +27,50 @@ def tazArticles():
     divs = soup.find_all("div", "sect_shop")
     articles = divs[0].find_all("a")
 
-    messageText = ""
     for a in articles:
-        urll = str(a.get('href'))
-        if urll != "None":
-            link = url+urll
+        try:
+            messageText = ""
+            urll = str(a.get('href'))
+            
+            if urll != "None":
+                title = a.h4.text
+                link = url+urll
 
-            website = requests.get(link)
-            soup = BeautifulSoup(website.content, features="html.parser")
+                website = requests.get(link)
+                soup = BeautifulSoup(website.content, features="html.parser")
 
-            subtitle = soup.find_all("p", "intro")
-            messageText += f"{subtitle[0].text} \n{link} \n\n"
-    return messageText
-
-token = os.environ["telegramToken"]
-bot = telegram.Bot(token=token)
-print(bot.get_me())
-print(datetime.datetime.now())
+                subtitle = soup.find_all("p", "intro")
+                messageText += f"**{title}**\n{subtitle[0].text} \n{link}\n"
+                if title not in articles:
+                    COLLECTION[title] = messageText
+        except Exception as e:
+            print()
+            print(f"ERROR: {e}")
 
 def send():
-    print("Send message")
-    bot.send_message("@taztopstories", tazArticles())
+    print("Sending...")
 
-# schedule.every(10).minutes.do(send)
-schedule.every().day.at("17:30").do(send)
+    global COLLECTION
+    count = 1
+    message = ""
 
+    for article, text in reversed(COLLECTION.items()):
+
+        message += text
+
+        # Makes sure it sends at most 8 messages
+        if count >= 8:
+            break
+        else:
+            count += 1
+
+    bot.send_message("@taztopstories", print(message))
+    COLLECTION = {}
+
+schedule.every(1).hour.do(scrape)
+schedule.every().day.at("17:40").do(send)
+
+scrape()
 while True:
     schedule.run_pending()
     time.sleep(600)
